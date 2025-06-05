90ee393f2cc4d24d3de7b54d90228ee9b3d7ea1b
HanzalaJawwad <hanzala.jawwad@gmail.com>
2025-06-05 09:35:59 +0530

    10b.jl

---
+++ 10b.jl
@@ -0,0 +1,5 @@
+using Plots
+# Define the function y = sin(x) + sin(2x)
+f(x) = sin(x) + sin(2x)
+# Generate the plot
+plot(f, -2π, 2π, label="y = sin(x) + sin(2x)", title="Plot of y = sin(x) + sin(2x)")
